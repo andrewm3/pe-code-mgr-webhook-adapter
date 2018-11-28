9003baa4830a0706ab214a4c6ceda8b19b97ccf0
andrewm3 <2037905+andrewm3@users.noreply.github.com>
2018-11-29 08:57:54 +1100

    Add whitelist option which takes an array of branches

--- adapter/adapter.go
+++ adapter/adapter.go
@@ -61,7 +61,7 @@ func EventHandler(w http.ResponseWriter, r *http.Request, config HandlerConfig) 
 	}
 
 	for _, x := range config.Whitelist {
-		if event.Ref == x {
+		if event.Ref == fmt.Sprintf("refs/head/%s", x) {
 			allowed = true
 			break
 		}
--- main.go
+++ main.go
@@ -5,15 +5,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/andrewm3/pe-code-mgr-webhook-adapter/adapter"
 )
 
 var port int
 var redirect string
-var whitelist = []string{
-	"refs/heads/production",
-}
+var whitelist string
 
 func init() {
 	flag.IntVar(&port, "port", 8080, "The port to serve on")
@@ -23,13 +22,14 @@ func init() {
 		"https://localhost:8170/code-manager/v1/webhook",
 		"The URL for Code Manager, which requests will be forwarded to",
 	)
+	flag.StringVar(&whitelist, "whitelist", "production", "A comma-separated list of allowed branches")
 }
 
 func main() {
 	flag.Parse()
 	config := adapter.HandlerConfig{
 		Redirect:  redirect,
-		Whitelist: whitelist,
+		Whitelist: strings.Split(whitelist, ","),
 	}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		adapter.EventHandler(w, r, config)
